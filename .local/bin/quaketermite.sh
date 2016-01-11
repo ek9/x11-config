178b4e8e3a6c3e1687cf6c943ea6bdaad936f7d6
ek9 <dev@ek9.co>
2016-01-11 15:10:59 +0100

    update gitingnore + add bins

---
+++ .local/bin/quaketermite.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+if command -v tdrop >/dev/null 2>&1; then
+    tdrop -a -m -s quake -y 16 -x 6 -w 99% termite
+else
+    xdotool search --onlyvisible --classname QuakeTermite windowunmap \
+        || xdotool search --classname QuakeTermite windowmap \
+        || termite --name QuakeTermite --geometry 1360x300+0+20
+fi
+
