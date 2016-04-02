03936d2a194e07b622c813057e3ae0e762b04b35
ek9 <dev@ek9.co>
2016-04-02 11:33:56 +0200

    xprofile: set default programs

---
+++ .config/X11/profile.d/05-programs.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env bash
+## ek9/x11-config - https://github.com/ek9/x11-config
+## 05-programs.sh
+## This file sets up default programs for X11
+
+[[ -x $(command -v firefox) ]] && export BROWSER=firefox
+
