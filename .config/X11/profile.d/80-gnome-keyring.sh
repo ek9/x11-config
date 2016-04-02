84cef6fb299c3fc293359000c58279dc0b8171cc
ek9 <dev@ek9.co>
2016-04-02 15:19:30 +0200

    xprofile: add gnome-keyring support for i3wm

---
+++ .config/X11/profile.d/80-gnome-keyring.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+## ek9/x11-config - https://github.com/ek9/x11-config
+## 80-gnome-keyring.sh
+## This file sets up gnome keyring
+
+# Export session for gnome-keyring
+export DESKTOP_SESION=gnome
+
