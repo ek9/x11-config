728f751464e6d1ac572294b6b7ac983a113a049a
ek9 <dev@ek9.co>
2018-03-04 23:32:33 +0000

    xautolock: use physlock

--- .config/X11/xinitrc.d/95-xautolock.sh
+++ .config/X11/xinitrc.d/95-xautolock.sh
@@ -2,6 +2,7 @@
 ## ek9/x11-config - https://github.com/ek9/x11-config
 ## 95-xautolock.sh
 
+LOCKER=physlock
 if command -v xautolock >/dev/null 2>&1; then
-    xautolock -time 30 -locker slock -notify 30 -notifier "notify-send -u critical -t 10000 -- 'Locking screen in 30'" &
+    xautolock -time 15 -locker $LOCKER -notify 30 -notifier "notify-send -u critical -t 10000 -- 'Locking screen in 30'" &
 fi
