70bbe47a1ff15f24675d122cad115dac348743dc
ek9 <dev@ek9.co>
2018-03-04 23:13:10 +0000

    nitrogen update

--- .config/X11/xinitrc.d/75-nitrogen.sh
+++ .config/X11/xinitrc.d/75-nitrogen.sh
@@ -2,7 +2,7 @@
 ## ek9/x11-config - https://github.com/ek9/x11-config
 ## 75-nitrogen.sh
 
-if -d ~/.local/share/wallpaper && command -v nitrogen >/dev/null 2>&1; then
-    nitrogen --set-auto --random ~/.local/share/wallpaper/
+if [ -d ~/.local/share/wallpaper ] && command -v nitrogen >/dev/null 2>&1; then
+    nitrogen --set-scaled --random ~/.local/share/wallpaper/
 fi
 
